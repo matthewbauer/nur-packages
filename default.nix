5283ca066b3ea58c7d9ed34a2464e771a9d6e544
Matthew Bauer <mjbauer95@gmail.com>
2018-07-09 14:31:52 -0400

    Initial commit

---
+++ default.nix
@@ -0,0 +1,56 @@
+{ pkgs ? import <nixpkgs> (builtins.removeAttrs args ["pkgs"] )
+, config ? null, localSystem ? null, crossSystem ? null, system ? null
+, ... } @ args:
+
+let
+  inherit (pkgs) callPackage;
+
+  self = {
+    tensorflow-bin = pkgs.pythonPackages.callPackage ./tensorflow-bin {};
+    firefox-bin = callPackage ./firefox-bin {
+      gconf = pkgs.gnome2.GConf;
+      inherit (pkgs.gnome2) libgnome libgnomeui;
+      inherit (pkgs.gnome3) defaultIconTheme;
+    };
+    tor-browser-bundle-bin = callPackage ./tor-browser-bundle-bin {};
+  };
+
+  aliases = {
+    torbrowser = self.tor-browser-bundle-bin;
+  };
+
+  firefox-devedition-bin-unwrapped = self.firefox-bin.override {
+    channel = "devedition";
+    generated = import ./firefox-bin/devedition_sources.nix;
+  };
+
+  firefox-beta-bin-unwrapped = self.firefox-bin.override {
+    channel = "beta";
+    generated = import ./firefox-bin/beta_sources.nix;
+  };
+
+  overrides = rec {
+    firefox-bin = pkgs.wrapFirefox self.firefox-bin {
+      browserName = "firefox";
+      name = "firefox-bin-" +
+        (builtins.parseDrvName self.firefox-bin.name).version;
+      desktopName = "Firefox";
+    };
+
+    firefox-beta-bin = pkgs.wrapFirefox firefox-beta-bin-unwrapped {
+      browserName = "firefox";
+      name = "firefox-beta-bin-" +
+        (builtins.parseDrvName firefox-beta-bin-unwrapped.name).version;
+      desktopName = "Firefox Beta";
+    };
+
+    firefox-devedition-bin = pkgs.wrapFirefox firefox-devedition-bin-unwrapped {
+      browserName = "firefox";
+      nameSuffix = "-devedition";
+      name = "firefox-devedition-bin-" +
+        (builtins.parseDrvName firefox-devedition-bin-unwrapped.name).version;
+      desktopName = "Firefox DevEdition";
+    };
+  };
+
+in self // overrides // aliases
